9161e16710ebb35f64236350c2cdc85ee1a8d3bb
Jason Novich <jasonnovich@o2pm.com>
2019-09-09 16:09:12 +0300

    new master

---
+++ src/helloWorld2.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Jason Novich and O2 Product Management.
+ *
+ * Authored by Jason Novich
+ *
+ * All Rights Reserved
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied, unless otherwise agreed upon by Jason Novich or a representstive of O2 Product Management.
+ *
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+public class helloWorld2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
